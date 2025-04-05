4464c87af04cb047d2bc9bcdde0d311527acd665
Sashavav <vav110011@gmail.com>
2025-04-05 15:33:27 +0300

    Update styles

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ const observer = new IntersectionObserver((entries) => {
     });
 }, { threshold: 0.1 });
 
-document.querySelectorAll('section').forEach(section => {
-    observer.observe(section);
+document.querySelectorAll('section, .section-title, .project-item, .skills-column, footer').forEach(el => {
+    observer.observe(el);
 });
 
 // Initialize
